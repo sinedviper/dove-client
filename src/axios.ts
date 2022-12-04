0c752dddbac53aa4f105fda9249ae8a554490409
thewoodman999 <119786879+thewoodman999@users.noreply.github.com>
2022-12-04 18:58:56 +0200

    fix bugs

--- src/axios.ts
+++ src/axios.ts
@@ -8,8 +8,8 @@ const instance = axios.create({
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem("token");
-  config.headers["Access-Control-Allow-Origin"] =
-    "https://dove-client.vercel.app";
+  config.headers["Access-Control-Allow-Origin"] = "*";
+  config.headers["Access-Control-Allow-Headers"] = "*";
   return config;
 });
 
--- src/index.tsx
+++ src/index.tsx
@@ -43,7 +43,8 @@ const authLink = setContext(() => {
   // return the headers to the context so httpLink can read them
   return {
     headers: {
-      "Access-Control-Allow-Origin": "https://dove-client.vercel.app",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
       authorization: token && token,
     },
   };
